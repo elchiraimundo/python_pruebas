6f8e7d89acb31064bf7256ed1b7a54df39684497
elchiraimundo <raimundo_cristian@hotmail.com>
2023-10-30 12:11:13 -0300

    Saludo buen dia

--- prueba online/app.py
+++ prueba online/app.py
@@ -11,5 +11,10 @@ def saludar():
     nombre = request.form['nombre']
     return f'Hola {nombre}!'
 
+@app.route('/buen_dia', methods=['POST'])
+def saludo_buen_dia():
+    nombre = request.form['nombre']
+    return f'Buen dia {nombre}!'
+
 if __name__ == '__main__':
     app.run()
