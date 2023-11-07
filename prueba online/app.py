294dc1d461b2528b53fcd7a3a6aaf3880b10ceab
elchiraimundo <raimundo_cristian@hotmail.com>
2023-11-07 10:44:27 -0300

    Como estas?!

--- prueba online/app.py
+++ prueba online/app.py
@@ -14,12 +14,12 @@ def saludar():
 @app.route('/buen_dia', methods=['POST'])
 def saludo_buen_dia():
     nombre = request.form['nombre']
-    return f'Buen dia {nombre}!'
+    return f'Buen dia. Como estas {nombre}!'
 
 @app.route('/buenas_noche', methods=['POST'])
 def saludo_buenas_noche():
     nombre = request.form['nombre']
-    return f'Buen dia {nombre}!'
+    return f'Buen dia. Como estas {nombre}!'
 
 if __name__ == '__main__':
     app.run()
